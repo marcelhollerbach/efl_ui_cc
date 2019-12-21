b57e2d185380d5cbb5a3e2d1f3b70444ab9dd3ca
Marcel Hollerbach <mail@marcel-hollerbach.de>
2019-12-21 18:13:22 +0100

    validator: improve this
    it now gives a bit of context where the error is.

--- src/lib/validator.c
+++ src/lib/validator.c
@@ -4,10 +4,49 @@
 
 typedef struct {
    Eolian_State *s;
+   Eina_Array *messages;
 } Validator_Context;
 
+typedef struct {
+   const char *placment_context;
+   const Efl_Ui_Node *node;
+} Validator_Context_Message_Node;
 
-static Eina_Bool validate_node(Validator_Context *ctx, Efl_Ui_Node *node);
+static Eina_Bool validate_node(Validator_Context *ctx, Efl_Ui_Node *node, const char *placment_context);
+
+#define ERROR_OUT(ctx, ...) \
+  do { \
+    _print_position(ctx); \
+    printf(__VA_ARGS__); \
+    return EINA_FALSE; \
+  } while (0) \
+
+static void
+_print_position(const Validator_Context *ctx)
+{
+   Eina_Iterator *msgs = eina_array_iterator_new(ctx->messages);
+   Validator_Context_Message_Node *node;
+   Eina_Bool first = EINA_TRUE;
+
+   EINA_ITERATOR_FOREACH(msgs, node)
+     {
+        const char *opener = "in";
+
+        if (!first)
+          printf(", ");
+        else
+          opener = "In";
+
+        printf("%s ", node->placment_context);
+        if (node->node->id)
+          printf("%s \"%s\"", opener, node->node->id);
+        else
+          printf("%s \"%s\"", opener, node->node->type);
+        first = EINA_FALSE;
+     }
+   eina_iterator_free(msgs);
+   printf(":\n");
+}
 
 static Eina_Bool
 validate_builtin_property_values(const Validator_Context *ctx, const Eolian_Type *type, const Efl_Ui_Property_Value *value)
@@ -16,43 +55,52 @@ validate_builtin_property_values(const Validator_Context *ctx, const Eolian_Type
 
    if (builtin >= EOLIAN_TYPE_BUILTIN_BYTE && builtin <= EOLIAN_TYPE_BUILTIN_PTRDIFF)
      {
-        EINA_SAFETY_ON_TRUE_RETURN_VAL(value->is_node, EINA_FALSE);
         char *tmp_end;
+        if (value->is_node)
+          {
+            ERROR_OUT(ctx, "Expected a number");
+          }
         strtol(value->value, &tmp_end, 10);
         if (errno == EINVAL || errno == ERANGE || tmp_end != value->value + strlen(value->value))
           {
-             printf("Numeric values must be numbers: '%s'\n", value->value);
-             return EINA_FALSE;
+             ERROR_OUT(ctx, "Numeric values must be numbers: '%s'\n", value->value);
           }
      }
    else if (builtin >= EOLIAN_TYPE_BUILTIN_FLOAT && builtin <= EOLIAN_TYPE_BUILTIN_DOUBLE)
      {
-        EINA_SAFETY_ON_TRUE_RETURN_VAL(value->is_node, EINA_FALSE);
         char *tmp_end;
+        if (value->is_node)
+          {
+            ERROR_OUT(ctx, "Expected a float");
+          }
         strtof(value->value, &tmp_end);
         if (errno == ERANGE || tmp_end != value->value + strlen(value->value))
           {
-             printf("FLoating values must be numbers: '%s'\n", value->value);
-             return EINA_FALSE;
+             ERROR_OUT(ctx, "FLoating values must be numbers: '%s'\n", value->value);
           }
+
      }
    else if (builtin == EOLIAN_TYPE_BUILTIN_BOOL)
      {
-        EINA_SAFETY_ON_TRUE_RETURN_VAL(value->is_node, EINA_FALSE);
+        if (value->is_node)
+          {
+            ERROR_OUT(ctx, "Expected true or false");
+          }
         if (!eina_streq(value->value, "true") && !eina_streq(value->value, "false"))
           {
-             printf("Boolean values must be true or false, not '%s'\n", value->value);
-             return EINA_FALSE;
+             ERROR_OUT(ctx, "Boolean values must be true or false, not '%s'\n", value->value);
           }
      }
    else if (builtin >= EOLIAN_TYPE_BUILTIN_MSTRING && builtin <= EOLIAN_TYPE_BUILTIN_STRINGSHARE)
      {
-        EINA_SAFETY_ON_TRUE_RETURN_VAL(value->is_node, EINA_FALSE);
+        if (value->is_node)
+          {
+            ERROR_OUT(ctx, "Expected a String");
+          }
      }
    else
      {
-        printf("Type %d is not handled yet.\n", builtin);
-        return EINA_FALSE;
+        ERROR_OUT(ctx, "Type %d is not handled yet.\n", builtin);
      }
    return EINA_TRUE;
 }
@@ -78,8 +126,7 @@ validate_enum_values(const Validator_Context *ctx, const Eolian_Typedecl *type, 
         return EINA_TRUE;
      }
 
-   printf("Value %s not within '%s'\n", value->value, eina_strbuf_release(buf));
-
+   ERROR_OUT(ctx, "Value %s not within '%s'\n", value->value, eina_strbuf_release(buf));
    eina_iterator_free(iter);
    return EINA_FALSE;
 }
@@ -94,8 +141,7 @@ validate_property(Validator_Context *ctx, const Eolian_Class *klass, Efl_Ui_Prop
    f = find_function(ctx->s, klass, node->key);
    if (!f)
      {
-        printf("Function %s not found\n", node->key);
-        return EINA_FALSE;
+        ERROR_OUT(ctx, "Function %s not found\n", node->key);
      }
    parameters = eolian_property_values_get(f, EOLIAN_PROP_SET);
    int c = 0;
@@ -118,22 +164,31 @@ validate_property(Validator_Context *ctx, const Eolian_Class *klass, Efl_Ui_Prop
                   if (decl && eolian_typedecl_type_get(decl) == EOLIAN_TYPEDECL_ENUM)
                     {
                        if (!validate_enum_values(ctx, decl, value))
-                         return EINA_FALSE;
+                         {
+                            eina_iterator_free(parameters);
+                            return EINA_FALSE;
+                         }
                     }
                   else
                     {
                        if (!validate_builtin_property_values(ctx, type, value))
                          {
-                            printf("Error, in line %s\n", node->key);
+                            eina_iterator_free(parameters);
                             return EINA_FALSE;
                          }
                     }
                }
              break;
-             case EOLIAN_TYPE_CLASS:
+             case EOLIAN_TYPE_CLASS: {
+               char buffer[100];
                EINA_SAFETY_ON_FALSE_RETURN_VAL(value->is_node, EINA_FALSE);
-               if (!validate_node(ctx, value->node))
-                 return EINA_FALSE;
+               snprintf(buffer, sizeof(buffer), "at property %s", node->key);
+               if (!validate_node(ctx, value->node, buffer))
+                 {
+                    eina_iterator_free(parameters);
+                    return EINA_FALSE;
+                 }
+             }
              break;
              default:
                EINA_LOG_ERR("This is an error");
@@ -143,23 +198,57 @@ validate_property(Validator_Context *ctx, const Eolian_Class *klass, Efl_Ui_Prop
         c++;
      }
    eina_iterator_free(parameters);
-   EINA_SAFETY_ON_FALSE_RETURN_VAL(c == eina_array_count(node->value), EINA_FALSE);
+   if (c != eina_array_count(node->value))
+     {
+        ERROR_OUT(ctx, "Not enough arguments for property %s", node->key);
+     }
 
    return EINA_TRUE;
 }
 
 static Eina_Bool
-validate_node(Validator_Context *ctx, Efl_Ui_Node *node)
+validate_klass(Validator_Context *ctx, const char *klass_name, const Eolian_Class *klass)
+{
+   if (!klass)
+     {
+        ERROR_OUT(ctx, "type %s cannot be found", klass_name);
+     }
+   return EINA_TRUE;
+}
+
+static Eina_Bool
+validate_part_name(Validator_Context *ctx, Efl_Ui_Pack_Pack *pack, Eina_Hash *part_names)
+{
+   if (!pack->part_name)
+     {
+        ERROR_OUT(ctx, "Part name required");
+     }
+   if (eina_hash_find(part_names, &pack->part_name))
+     {
+        ERROR_OUT(ctx, "Part name %s is duplicated", pack->part_name);
+     }
+   eina_hash_add(part_names, &pack->part_name, pack);
+   return EINA_TRUE;
+}
+
+static Eina_Bool
+validate_node(Validator_Context *ctx, Efl_Ui_Node *node, const char *placment_context)
 {
    Eina_Iterator *it;
    Efl_Ui_Property *property;
    const Eolian_Class *klass;
+   Validator_Context_Message_Node ctx_node;
+
+   ctx_node.placment_context = placment_context;
+   ctx_node.node = node;
+
+   eina_array_push(ctx->messages, &ctx_node);
 
    //validate type here
    klass = find_klass(ctx->s, node->type);
-   if (!klass)
+   if (!validate_klass(ctx, node->type, klass))
      {
-        printf("Failed to find klass\n");
+        eina_array_pop(ctx->messages);
         return EINA_FALSE;
      }
 
@@ -170,6 +259,7 @@ validate_node(Validator_Context *ctx, Efl_Ui_Node *node)
         if (!validate_property(ctx, klass, property))
           {
              eina_iterator_free(it);
+             eina_array_pop(ctx->messages);
              return EINA_FALSE;
           }
      }
@@ -181,14 +271,20 @@ validate_node(Validator_Context *ctx, Efl_Ui_Node *node)
      {
         Efl_Ui_Pack_Linear *linear;
         Eina_Iterator *it = eina_array_iterator_new(node->children_linear);
+        int i = 1;
 
         EINA_ITERATOR_FOREACH(it, linear)
           {
-             if (!validate_node(ctx, linear->basic.node))
+             char buffer[100];
+
+             snprintf(buffer, sizeof(buffer), "at %d. place", i);
+             if (!validate_node(ctx, linear->basic.node, buffer))
                {
                   eina_iterator_free(it);
+                  eina_array_pop(ctx->messages);
                   return EINA_FALSE;
                }
+             i ++;
           }
         eina_iterator_free(it);
      }
@@ -199,9 +295,13 @@ validate_node(Validator_Context *ctx, Efl_Ui_Node *node)
 
         EINA_ITERATOR_FOREACH(it, table)
           {
-             if (!validate_node(ctx, table->basic.node))
+             char buffer[100];
+
+             snprintf(buffer, sizeof(buffer), "at position (%s, %s, %s, %s)", table->x, table->y, table->w, table->h);
+             if (!validate_node(ctx, table->basic.node, buffer))
                {
                   eina_iterator_free(it);
+                  eina_array_pop(ctx->messages);
                   return EINA_FALSE;
                }
           }
@@ -211,23 +311,33 @@ validate_node(Validator_Context *ctx, Efl_Ui_Node *node)
      {
         Efl_Ui_Pack_Pack *pack;
         Eina_Iterator *it = eina_array_iterator_new(node->children_table);
+        Eina_Hash *part_name = eina_hash_string_small_new(NULL);
 
         EINA_ITERATOR_FOREACH(it, pack)
           {
-             if (!validate_node(ctx, pack->basic.node))
+             char buffer[100];
+
+             if (!validate_part_name(ctx, pack, part_name))
                {
+                  eina_hash_free(part_name);
                   eina_iterator_free(it);
+                  eina_array_pop(ctx->messages);
                   return EINA_FALSE;
                }
-             if (!pack->part_name)
+             snprintf(buffer, sizeof(buffer), "at part %s", pack->part_name);
+             if (!validate_node(ctx, pack->basic.node, buffer))
                {
+                  eina_hash_free(part_name);
                   eina_iterator_free(it);
-                  printf("Part name required\n");
+                  eina_array_pop(ctx->messages);
                   return EINA_FALSE;
                }
+
           }
+        eina_hash_free(part_name);
         eina_iterator_free(it);
      }
+   eina_array_pop(ctx->messages);
    return EINA_TRUE;
 }
 
@@ -236,11 +346,17 @@ validate(Eolian_State *state, Efl_Ui *ui)
 {
    Validator_Context ctx = {state};
 
+   ctx.messages = eina_array_new(10);
    if (!ui)
      return EINA_TRUE;
 
    if (ui->content)
-     return validate_node(&ctx, ui->content);
+     {
+        Eina_Bool v = validate_node(&ctx, ui->content, "");
+        EINA_SAFETY_ON_FALSE_RETURN_VAL(eina_array_count(ctx.messages) == 0, EINA_FALSE);
+        eina_array_free(ctx.messages);
+        return v;
+     }
    else
      return EINA_TRUE;
 }
