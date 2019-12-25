ab51df2dbeea091ca7dea90db99e86695c49bf4f
Marcel Hollerbach <mail@marcel-hollerbach.de>
2019-12-25 21:33:30 +0100

    editor: improve predictive UI
    you can now search for types

--- src/efl_ui_editor/predictor_ui.c
+++ src/efl_ui_editor/predictor_ui.c
@@ -35,6 +35,36 @@ _types_selection_changed_cb(void *data, const Efl_Event *ev)
    eina_promise_resolve(_promise_remove(data), resolved_value);
 }
 
+static void
+_text_changed_cb(void *data, const Efl_Event *ev)
+{
+   Predictor_Ui_Data *d = data;
+
+   const char *text = efl_text_get(ev->object);
+
+   efl_pack_clear(d->container);
+   Predicted_Class *klass = get_available_types();
+   for (int i = 0; klass[i].klass_name; ++i)
+     {
+        if (text)
+          {
+             char *atext = eina_strdup(text);
+             char *klass_name = eina_strdup(klass[i].klass_name);
+
+             eina_str_tolower(&atext);
+             eina_str_tolower(&klass_name);
+
+             if (strstr(klass_name, atext) == NULL)
+               continue;
+          }
+
+        Efl_Ui_Default_Item *item = efl_add(EFL_UI_LIST_DEFAULT_ITEM_CLASS, d->container);
+        efl_gfx_hint_size_min_set(item, EINA_SIZE2D(80, 50));
+        efl_text_set(item, klass[i].klass_name);
+        efl_pack_end(d->container, item);
+     }
+}
+
 Eina_Future*
 select_available_types(void)
 {
@@ -49,9 +79,8 @@ select_available_types(void)
         efl_gfx_hint_size_min_set(data->container, EINA_SIZE2D(250, 250));
         efl_event_callback_add(data->root, EFL_UI_POPUP_EVENT_BACKWALL_CLICKED, _types_hide_cb, NULL);
         efl_event_callback_add(data->container, EFL_UI_SELECTABLE_EVENT_SELECTION_CHANGED, _types_selection_changed_cb, data->root);
-
+        efl_event_callback_add(data->search_text, EFL_TEXT_INTERACTIVE_EVENT_CHANGED_USER, _text_changed_cb, data);
         klass = get_available_types();
-
         for (int i = 0; klass[i].klass_name; ++i)
           {
              Efl_Ui_Default_Item *item = efl_add(EFL_UI_LIST_DEFAULT_ITEM_CLASS, data->container);
@@ -59,7 +88,6 @@ select_available_types(void)
              efl_text_set(item, klass[i].klass_name);
              efl_pack_end(data->container, item);
           }
-        free(klass);
      }
    //bind promise to the popup
    EINA_SAFETY_ON_FALSE_RETURN_VAL(efl_key_data_get(data->root, "__promise") == NULL, NULL);
@@ -67,6 +95,9 @@ select_available_types(void)
 
    efl_ui_multi_selectable_all_unselect(data->container);
    efl_gfx_entity_visible_set(data->root, EINA_TRUE);
+   efl_ui_focus_util_focus(data->container);
+   efl_text_set(data->search_text, "");
+
 
    return eina_future_new(ctx);
 }
@@ -91,6 +122,36 @@ _selection_changed_cb(void *data, const Efl_Event *ev)
    efl_del(data);
 }
 
+static void
+_properties_changed_cb(void *data, const Efl_Event *ev)
+{
+   Property_Name_Ui_Data *d = data;
+   const char *text = efl_text_get(ev->object);
+
+   efl_pack_clear(d->container);
+   Predicted_Property *property = get_available_properties(efl_key_data_get(ev->object, "__node"));
+   for (int i = 0; property[i].name; ++i)
+     {
+        if (text)
+          {
+             char *atext = eina_strdup(text);
+             char *klass_name = eina_strdup(property[i].name);
+
+             eina_str_tolower(&atext);
+             eina_str_tolower(&klass_name);
+
+             if (strstr(klass_name, atext) == NULL)
+               continue;
+          }
+        Efl_Ui_Default_Item *item = efl_add(EFL_UI_LIST_DEFAULT_ITEM_CLASS, d->container);
+        efl_gfx_hint_size_min_set(item, EINA_SIZE2D(80, 50));
+        efl_text_set(item, property[i].name);
+        efl_pack_end(d->container, item);
+     }
+   free(property);
+
+}
+
 Eina_Future*
 select_available_properties(Efl_Ui_Node *node)
 {
@@ -100,20 +161,22 @@ select_available_properties(Efl_Ui_Node *node)
 
    for (int i = 0; property[i].name; ++i)
      {
-        Efl_Ui_Default_Item *item = efl_add(EFL_UI_LIST_DEFAULT_ITEM_CLASS, data->list);
+        Efl_Ui_Default_Item *item = efl_add(EFL_UI_LIST_DEFAULT_ITEM_CLASS, data->container);
         efl_gfx_hint_size_min_set(item, EINA_SIZE2D(80, 50));
         efl_text_set(item, property[i].name);
-        efl_pack_end(data->list, item);
+        efl_pack_end(data->container, item);
      }
    free(property);
 
+   efl_key_data_set(data->search_text, "__node", node);
+
    efl_event_callback_add(data->root, EFL_UI_POPUP_EVENT_BACKWALL_CLICKED, _backwall_clicked_cb, data->root);
-   efl_event_callback_add(data->list, EFL_UI_SELECTABLE_EVENT_SELECTION_CHANGED, _selection_changed_cb, data->root);
-   efl_gfx_hint_size_min_set(data->list, EINA_SIZE2D(250, 250));
+   efl_event_callback_add(data->container, EFL_UI_SELECTABLE_EVENT_SELECTION_CHANGED, _selection_changed_cb, data->root);
+   efl_event_callback_add(data->search_text, EFL_TEXT_INTERACTIVE_EVENT_CHANGED_USER, _properties_changed_cb, data);
+   efl_event_callback_add(data->root, EFL_EVENT_DEL, (Efl_Event_Cb)free, data);
+   efl_gfx_hint_size_min_set(data->container, EINA_SIZE2D(250, 250));
    efl_key_data_set(data->root, "__promise", ctx);
    efl_gfx_entity_visible_set(data->root, EINA_TRUE);
-
-   free(data);
 
    return eina_future_new(ctx);
 }
