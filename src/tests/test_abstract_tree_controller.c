6ba87ffeed59c7df5448be3309f015aa0796b041
Marcel Hollerbach <mail@marcel-hollerbach.de>
2019-12-27 10:52:22 +0100

    tests: check beta and none beta properties

--- src/tests/test_abstract_tree_controller.c
+++ src/tests/test_abstract_tree_controller.c
@@ -39,7 +39,6 @@ void setup(void)
    content = efl_ui_content_get(ui_tree);
    efl_ui_name_set(ui_tree, "<Insert Name Here>");
    node_type_set(content, "Efl.Ui.Button");
-
 }
 
 void teardown(void)
@@ -92,10 +91,11 @@ START_TEST(test_del_child)
 }
 END_TEST
 
-START_TEST(set_all_properties_on_all_types)
+START_TEST(set_all_properties_on_all_types_true)
 {
    //first add a property that gets removed
    const Predicted_Class *klasses = get_available_types();
+   eolian_bridge_beta_allowed_set(EINA_TRUE);
 
    for (int i = 0; klasses[i].klass_name; ++i)
      {
@@ -117,6 +117,38 @@ START_TEST(set_all_properties_on_all_types)
 }
 END_TEST
 
+START_TEST(set_all_properties_on_all_types_false)
+{
+   //first add a property that gets removed
+   Eina_Hash *string_already_checked = eina_hash_string_small_new(NULL);
+   const Predicted_Class *klasses = get_available_types();
+   eolian_bridge_beta_allowed_set(EINA_FALSE);
+
+   for (int i = 0; klasses[i].klass_name; ++i)
+     {
+        change_type(content, klasses[i].klass_name);
+        const Predicted_Property *properties;
+        properties = get_available_properties(content);
+
+        for (int f = 0; properties[f].name; ++f)
+          {
+             if (eina_hash_find(string_already_checked, properties[f].name))
+               continue;
+             add_property(content, properties[f].name);
+          }
+
+        for (int f = 0; properties[f].name; ++f)
+          {
+             if (eina_hash_find(string_already_checked, properties[f].name))
+               continue;
+             del_property(content, properties[f].name);
+             eina_hash_add(string_already_checked, properties[f].name, content);
+          }
+        free((void*)properties);
+     }
+}
+END_TEST
+
 Suite * predictor_suite(void)
 {
     Suite *s;
@@ -132,8 +164,9 @@ Suite * predictor_suite(void)
     tcase_add_test(tc_core, test_add_table_child);
     tcase_add_test(tc_core, test_add_part_child);
     tcase_add_test(tc_core, test_del_child);
-    tcase_add_test(tc_core, set_all_properties_on_all_types);
-    tcase_set_timeout(tc_core, 20.0);
+    tcase_add_test(tc_core, set_all_properties_on_all_types_true);
+    tcase_add_test(tc_core, set_all_properties_on_all_types_false);
+    tcase_set_timeout(tc_core, 100.0);
 
     tcase_add_checked_fixture(tc_core, setup, teardown);
     suite_add_tcase(s, tc_core);
