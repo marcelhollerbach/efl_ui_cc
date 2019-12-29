3c075146d6e9c4ac0c35f9b7de4bcdecee61373a
Marcel Hollerbach <mail@marcel-hollerbach.de>
2019-12-29 11:52:16 +0100

    editor: display content in inline object

--- src/efl_ui_editor/display_ui.c
+++ src/efl_ui_editor/display_ui.c
@@ -11,6 +11,8 @@
 
 static Efl_Gfx_Entity *sample = NULL;
 
+static Efl_Ui_Win *internal = NULL;
+
 static void
 _geom_changed_cb(void *data, const Efl_Event *ev)
 {
@@ -21,12 +23,15 @@ _geom_changed_cb(void *data, const Efl_Event *ev)
    geom.w -= 40;
    geom.h -= 40;
 
-   efl_gfx_entity_geometry_set(sample, geom);
+   efl_gfx_entity_geometry_set(elm_win_inlined_image_object_get(internal), geom);
+   efl_gfx_entity_size_set(internal, geom.size);
 }
 
 void
 display_ui_init(Efl_Ui_Win *win EINA_UNUSED)
 {
+   internal = elm_win_add(win, "internal", ELM_WIN_INLINED_IMAGE);
+   elm_win_alpha_set(internal, EINA_TRUE);
    efl_event_callback_add(background, EFL_GFX_ENTITY_EVENT_POSITION_CHANGED, _geom_changed_cb, NULL);
    efl_event_callback_add(background, EFL_GFX_ENTITY_EVENT_SIZE_CHANGED, _geom_changed_cb, NULL);
 }
@@ -37,8 +42,8 @@ display_ui_refresh(Efl_Ui *ui)
    if (sample)
      efl_del(sample);
 
-   sample = object_generator(win, editor_state, ui);
-
-   efl_canvas_group_member_add(efl_canvas_object_render_parent_get(background), sample);
+   sample = object_generator(internal, editor_state, ui);
+   elm_object_tree_focus_allow_set(sample, EINA_FALSE);
+   elm_win_resize_object_add(internal, sample);
    _geom_changed_cb(NULL, NULL);
 }
